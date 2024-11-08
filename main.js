56ea5702cbba01d267e551f06766639f25578c3e
vladZbar <vladzbaradskij24@gmail.com>
2024-11-09 01:41:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+new Swiper('.swiper', {
+  loop: true,
+  
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  slidesPerView: 1.25,
+  spaceBetween: 16,
+});
+
+let brands = document.querySelector('.brand-list');
+let button = document.querySelector('.brand-button');
+let buttonText = button.querySelector('p');
+let buttonArrow = button.querySelector('div');
+let count = 1;
+
+button.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  if (count < 2) {
+    buttonText.innerHTML = 'Скрыть';
+    brands.style.height = '250px';
+    buttonArrow.classList.add('brand-button__arrow--down');
+    count++;
+  } else if (count > 1) {
+    buttonText.innerHTML = 'Показать всё';
+    buttonArrow.classList.remove('brand-button__arrow--down');
+    count--;
+    brands.style.height = '162px';
+  }
+})
+
+
+
+
